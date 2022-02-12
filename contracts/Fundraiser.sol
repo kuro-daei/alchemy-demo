1f44b81a7f7705dfd1a0cf15ea4deabe15203d1c
Eiji Kuroda <e@96d.jp>
2022-02-12 11:04:05 +0900

    page 109

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,9 +1,33 @@
-pragma solidity >=0.4.21 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract Fundraiser {
+contract Fundraiser is Ownable {
   string public name;
+  string public url;
+  string public imageURL;
+  string public description;
 
-  constructor(string memory _name) public {
+  address payable public beneficiary;
+  address public custodian;
+
+  constructor(
+    string memory _name,
+    string memory _url,
+    string memory _imageURL,
+    string memory _description,
+    address payable _beneficiary,
+    address _custodian
+  ) public {
     name = _name;
+    url = _url;
+    imageURL = _imageURL;
+    description = _description;
+    beneficiary = _beneficiary;
+    transferOwnership(_custodian);
+  }
+
+  function setBeneficiary(address payable _beneficiary) public onlyOwner {
+    beneficiary = _beneficiary;
   }
 }
